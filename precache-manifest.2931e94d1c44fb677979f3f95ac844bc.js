f39e3e95f9973e0ff18e83be9faa07ad2debc19a
Aayush kapil <aayushkapil100@gmail.com>
2024-05-14 20:09:15 +0530

    Updates

---
+++ precache-manifest.2931e94d1c44fb677979f3f95ac844bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4433b5a034ecd130c883072681163d9",
+    "url": "/News-hub/index.html"
+  },
+  {
+    "revision": "685bbea0a2386fb40c18",
+    "url": "/News-hub/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "967cc3012168bd9b690b",
+    "url": "/News-hub/static/js/2.ea3e449a.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/News-hub/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "685bbea0a2386fb40c18",
+    "url": "/News-hub/static/js/main.b81080da.chunk.js"
+  },
+  {
+    "revision": "305b90d9a40ff24ce22a",
+    "url": "/News-hub/static/js/runtime~main.cf35df6c.js"
+  },
+  {
+    "revision": "5780b10b3880805e76a4d87ce14c8d6f",
+    "url": "/News-hub/static/media/spinner.5780b10b.gif"
+  }
+]);
